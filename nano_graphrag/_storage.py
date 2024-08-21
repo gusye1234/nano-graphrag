dae39c59a17f430222352b0b0ea537d49bc90a25
zhaoyu.hu <zhaoyu.hu@trip.com>
2024-08-21 17:12:37 +0800

    增加了faiss作为向量数据库

--- nano_graphrag/_storage.py
+++ nano_graphrag/_storage.py
@@ -128,69 +128,6 @@ class MilvusLiteStorge(BaseVectorStorage):
     
 
 @dataclass
-class FAISSStorage(BaseVectorStorage):
-
-    def __post_init__(self):
-        self._index_file_name = os.path.join(
-            self.global_config["working_dir"], f"{self.namespace}_faiss.index"
-        )
-        self._metadata_file_name = os.path.join(
-            self.global_config["working_dir"], f"{self.namespace}_metadata.pkl"
-        )
-        self._max_batch_size = self.global_config["embedding_batch_num"]
-        
-        if os.path.exists(self._index_file_name):
-            self._index = faiss.read_index(self._index_file_name)
-            with open(self._metadata_file_name, 'rb') as f:
-                self._metadata = pickle.load(f)
-        else:
-            self._index = faiss.IndexIDMap(faiss.IndexFlatIP(self.embedding_func.embedding_dim))
-            self._metadata = {}
-
-    async def upsert(self, data: dict[str, dict]):
-        logger.info(f"Inserting {len(data)} vectors to {self.namespace}")
-        
-        contents = [v["content"] for v in data.values()]
-        batches = [
-            contents[i : i + self._max_batch_size]
-            for i in range(0, len(contents), self._max_batch_size)
-        ]
-        embeddings_list = await asyncio.gather(
-            *[self.embedding_func(batch) for batch in batches]
-        )
-        embeddings = np.concatenate(embeddings_list)
-        
-        ids = []
-        for k, v in data.items():
-            metadata = {k1: v1 for k1, v1 in v.items() if k1 in self.meta_fields}
-            metadata['id'] = k
-            self._metadata[k] = metadata
-            ids.append(int(k) if k.isdigit() else hash(k))
-        
-        ids = np.array(ids, dtype=np.int64)
-        self._index.add_with_ids(embeddings, ids)
-        
-        faiss.write_index(self._index, self._index_file_name)
-        with open(self._metadata_file_name, 'wb') as f:
-            pickle.dump(self._metadata, f)
-        
-        return len(data)
-
-    async def query(self, query, top_k=5):
-        embedding = await self.embedding_func([query])
-        distances, indices = self._index.search(embedding, top_k)
-        
-        results = []
-        for i, (distance, idx) in enumerate(zip(distances[0], indices[0])):
-            if idx != -1:  # FAISS returns -1 for empty slots
-                id_str = str(idx)
-                if id_str in self._metadata:
-                    metadata = self._metadata[id_str]
-                    results.append({**metadata, "distance": 1 - distance})  # Convert to cosine distance
-        
-        return results
-
-@dataclass
 class NetworkXStorage(BaseGraphStorage):
     @staticmethod
     def load_nx_graph(file_name) -> nx.Graph:
