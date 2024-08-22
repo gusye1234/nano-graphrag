f968ed713a5acfcef52aca0a6e18d39e0c65597e
Gus <jianbaiye@outlook.com>
2024-08-22 15:26:18 +0800

    tests: update delete cache (#13)

--- examples/using_deepseek_as_llm.py
+++ examples/using_deepseek_as_llm.py
@@ -74,12 +74,12 @@ def insert():
     with open("./tests/mock_data.txt", encoding="utf-8-sig") as f:
         FAKE_TEXT = f.read()
 
-    remove_if_exist(f"{WORKING_DIR}/milvus_lite.db")
+    remove_if_exist(f"{WORKING_DIR}/vdb_entities.json")
     remove_if_exist(f"{WORKING_DIR}/kv_store_full_docs.json")
     remove_if_exist(f"{WORKING_DIR}/kv_store_text_chunks.json")
     remove_if_exist(f"{WORKING_DIR}/kv_store_community_reports.json")
     remove_if_exist(f"{WORKING_DIR}/graph_chunk_entity_relation.graphml")
-    half_len = len(FAKE_TEXT) // 2
+
     rag = GraphRAG(
         working_dir=WORKING_DIR,
         enable_llm_cache=True,
